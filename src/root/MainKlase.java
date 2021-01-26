5144b226f0ff9a0df1b52a7212ce8e3143a7af6c
Sandman^KFMF <sandmankfmf@gmail.com>
2021-01-26 12:09:00 +0200

    Sudėsiu ir klases

--- src/root/MainKlase.java
+++ src/root/MainKlase.java
@@ -4,7 +4,8 @@ public class MainKlase {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
 	}
 
 }
